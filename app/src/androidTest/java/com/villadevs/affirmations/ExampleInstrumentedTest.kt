8eb2f98f412ef864707df44232aa4880a1136ff0
Francisco Villanueva <pacovillano@hotmail.com>
2023-05-06 08:39:45 +0200

    Added Tests

--- app/src/androidTest/java/com/villadevs/affirmations/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/villadevs/affirmations/ExampleInstrumentedTest.kt
@@ -1,5 +1,13 @@
 package com.villadevs.affirmations
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +15,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +23,19 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class AffirmationsListTests {
+
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.villadevs.affirmations", appContext.packageName)
+    fun scroll_to_item() {
+        onView(withId(R.id.rvRecyclerView)).perform(
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(9)
+        )
+
+        onView(withText(R.string.affirmation10))
+            .check(matches(isDisplayed()))
     }
 }
--- app/src/test/java/com/villadevs/affirmations/ExampleUnitTest.kt
+++ app/src/test/java/com/villadevs/affirmations/ExampleUnitTest.kt
@@ -1,17 +1,29 @@
 package com.villadevs.affirmations
 
+import android.content.Context
+import com.villadevs.affirmations.adapter.ItemAdapter
+import com.villadevs.affirmations.model.Affirmation
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.mockito.Mockito.mock
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class AffirmationsAdapterTests {
+
+    private val context = mock(Context::class.java)
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
     }
 }
